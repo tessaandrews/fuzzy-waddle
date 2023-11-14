c789dcb9aa377c8f9f80bb4009720e1881c09cc2
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-11-13 20:31:26 -0600

    updated portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,17 +3,21 @@ export default function Portfolio() {
   return (
     <div className="header">
         <h1>Projects:</h1>
-        <div className="card">
-      <li className="list-group-item"><strong>Workday Calendar!</strong><a href="https://github.com/tessaandrews/reimagined-sniffle"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/tessaandrews/reimagined-sniffle" height= "300px" width= "1000px"></img></a></li>
+        <div className="card-sm">
+          <h2><strong>Workday Calendar!</strong></h2>
+      <li className="list-group-item"><a href="https://github.com/tessaandrews/reimagined-sniffle"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/tessaandrews/reimagined-sniffle" height= "300px" width= "fill"></img></a></li>
       </div>
-      <div className="card">
-        <li className="list-group-item"><strong>This Websites Repository!</strong><a href="https://github.com/tessaandrews/fuzzy-waddle"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/tessaandrews/fuzzy-waddle" height="300px"width="1000px" /></a></li>
+      <div className="card-sm">
+        <h2><strong>The Websites Repository!</strong></h2>
+        <li className="list-group-item"><a href="https://github.com/tessaandrews/fuzzy-waddle"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/tessaandrews/fuzzy-waddle" height="300px"width="fill" /></a></li>
         </div>
-        <div className="card">
-          <li className="list-group-item"><strong>Interactive Pet Api Website!</strong><a href="https://github.com/Vin7ag3/team2"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/Vin7ag3/team2" height="300px" width="1000px" /></a></li>
+        <div className="card-sm">
+          <h2><strong>Interactive Pet Api Website!</strong></h2>
+          <li className="list-group-item"><a href="https://github.com/Vin7ag3/team2"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/Vin7ag3/team2" height="300px" width="fill" /></a></li>
           </div>
-          <div className="card">
-            <li className="list-group-item"><strong>Blog Website!</strong><a href="https://github.com/AndrewAllen93/super-spoon"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/AndrewAllen93/super-spoon" height="300px" width="1000px" /></a></li>
+          <div className="card-sm">
+            <h2><strong>Blog Website!</strong></h2>
+            <li className="list-group-item"><a href="https://github.com/AndrewAllen93/super-spoon"><img src='https://legacy.reactjs.org/logo-og.png' onClick="https://github.com/AndrewAllen93/super-spoon" height="300px" width="fill" /></a></li>
               <div>
               </div>
             </div>
